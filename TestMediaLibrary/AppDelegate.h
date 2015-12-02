e62c4e4c03c7d667c4d6b4bb92171e865c71950f
222 111 <58583c@anarl-mac.local>
2015-12-02 09:49:59 +0800

    Initial Commit

---
+++ TestMediaLibrary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestMediaLibrary
+//
+//  Created by AnarL on 12/2/15.
+//  Copyright © 2015 AnarL. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestMediaLibrary/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TestMediaLibrary
+//
+//  Created by AnarL on 12/2/15.
+//  Copyright © 2015 AnarL. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
